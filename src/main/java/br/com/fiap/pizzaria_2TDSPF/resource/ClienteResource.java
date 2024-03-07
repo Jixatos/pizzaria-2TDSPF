94060f028cc3497d33daafbea249df0590d226b3
jixatos <j.gody2004@gmail.com>
2024-03-07 11:41:39 -0300

    Criação de novas classes e métodos GET, POST e DELETE do Cliente, mais GET do Cliente por ID

--- src/main/java/br/com/fiap/pizzaria_2TDSPF/resource/ClienteResource.java
+++ src/main/java/br/com/fiap/pizzaria_2TDSPF/resource/ClienteResource.java
@@ -2,22 +2,39 @@ package br.com.fiap.pizzaria_2TDSPF.resource;
 
 import br.com.fiap.pizzaria_2TDSPF.entity.Cliente;
 import br.com.fiap.pizzaria_2TDSPF.repositorio.ClienteRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
-
+    @Autowired
     private ClienteRepository repo;
 
 
     @GetMapping
     public List<Cliente> findAll() {
-
         return repo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Cliente findById(@PathVariable Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    @PostMapping
+    public Cliente save(@RequestBody Cliente cliente) {
+      return repo.save(cliente);
+    };
+
+    @Transactional
+    @DeleteMapping(value="/{id}")
+    public void delete(@PathVariable Long id) {
+        Cliente cliente = repo.findById(id).get();
+        repo.delete(cliente);
     }
 }
